cfd77a0494efbea6f70328fd832fbdd4edcd26bc
PereMarquez-web <pere.marquez@icloud.com>
2025-03-18 14:44:10 +0000

    Header dropdown

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,42 @@
 console.log("Hi, I am a script file");
 
 document.addEventListener('DOMContentLoaded', function() {
-    const searchToggle = document.getElementById('search-toggle');
-    const searchDropdown = document.getElementById('search-dropdown');
-    const overlay = document.getElementById('overlay'); 
+  const searchToggle = document.getElementById('search-toggle');
+  const searchDropdown = document.getElementById('search-dropdown');
+  const overlay = document.getElementById('overlay');
+  let hideTimeout;
 
     // Show dropdown on hover (mouseenter) and hide on mouseleave
     searchToggle.addEventListener('mouseenter', function() {
-        searchDropdown.classList.remove('hidden');
-        overlay.classList.remove('hidden');
+      if (hideTimeout) clearTimeout(hideTimeout);
+      searchDropdown.classList.remove('hidden');
+      overlay.classList.remove('hidden');
     });
 
     searchToggle.addEventListener('mouseleave', function() {
-        setTimeout(() => {
-            searchDropdown.classList.add('hidden');
-            overlay.classList.add('hidden');
-        }, 200);
+      hideTimeout = setTimeout(() => {
+        searchDropdown.classList.add('hidden');
+        overlay.classList.add('hidden');
+      }, 200);
     });
 
     //dropdown closes when you hover outside of the search bar
     searchDropdown.addEventListener('mouseenter', function() {
-        clearTimeout(hideDropdownTimeout);  // Prevent dropdown from hiding if hovering over it
+      if (hideTimeout) clearTimeout(hideTimeout);
     });
 
     searchDropdown.addEventListener('mouseleave', function() {
-        setTimeout(() => {
-            searchDropdown.classList.add('hidden');
-            overlay.classList.add('hidden');
-        }, 200);
+      hideTimeout = setTimeout(() => {
+        searchDropdown.classList.add('hidden');
+        overlay.classList.add('hidden');
+      }, 200);
     });
 
     // Optional: hide dropdown when clicking on the overlay
-    overlay.addEventListener("click", function() {
+    searchDropdown.addEventListener('mouseleave', function() {
+      hideTimeout = setTimeout(() => {
         searchDropdown.classList.add('hidden');
         overlay.classList.add('hidden');
+      }, 200);
     });
 });
-
